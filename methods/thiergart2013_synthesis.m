8d94c9f887985a7fede685e20206a0bcae86a050
Joe Tylka <josephgt@princeton.edu>
2018-11-17 19:00:00 -0500

    Implement ambRadialFilter from 3D3A-MATLAB-Toolbox

--- methods/thiergart2013_synthesis.m
+++ methods/thiergart2013_synthesis.m
@@ -64,7 +64,7 @@ for ii = 1:sf.numTimeFrames
         Gamma_ref = (1/sf.psi{refMic,ii}(kk)) - 1; % Eqs. (26) and (27)
         G_dir_ref = sqrt(Gamma_ref / (1 + Gamma_ref)); % Eq. (8)
         S_dir_ref = G_dir_ref * S_ref; % Eq. (7)
-        H_dir = conj(ambPointSource(0,sf.kVec(kk),norm(d_source))); % = conj((1/norm(d_source)) * exp(1i*sf.kVec(kk)*norm(d_source)));
+        H_dir = conj(ambRadialFilter(0,sf.kVec(kk),norm(d_source))); % = conj((1/norm(d_source)) * exp(1i*sf.kVec(kk)*norm(d_source)));
         S_dir(kk,ii) = S_dir_ref ./ H_dir; % Eq. (6)
         
         G_diff_ref = sqrt(1 / (1 + Gamma_ref)); % Eq. (14a)
@@ -85,34 +85,11 @@ for nn = 1:No
         for kk = 1:sf.specLen
             d_source = sf.s_0{ii,kk} - r;
             C_factor = ambSphericalHarmonicY(l, m, d_source, 'N3D');
-            H_dir = conj(ambPointSource(l,sf.kVec(kk),norm(d_source)));
+            H_dir = conj(ambRadialFilter(l,sf.kVec(kk),norm(d_source)));
             B_n(kk,ii) = (C_factor * H_dir * S_dir(kk,ii)) + (Q_factor * 1 * S_diff(kk,ii)); % Eq. (33), with H_diff = 1
         end
     end
     a_out(:,nn) = getInverseSTFT(B_n, sf.window, sf.noverlap, sf.nfft);
-end
-
-end
-
-function d = ambPointSource(l,k,r)
-
-if k(1)==0
-    DIM = find(size(k) ~= 1, 1, 'first');
-    k = k(2:end);
-    dropZero = true;
-    zeroVal = +~l;
-else
-    dropZero = false;
-end
-
-d = (1i^(l+1)*k).*sphericalHankelH(l,1,k*r);
-
-if dropZero
-    if ~isempty(DIM)
-        d = cat(DIM,zeroVal,d);
-    elseif dropZero && isempty(DIM)
-        d = zeroVal;
-    end
 end
 
 end
